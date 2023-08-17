39137830fd647b902e46a0a40fed1404c058d42c
OjiiAi <osv.garciaalvarez@gmail.com>
2023-08-17 05:03:36 +0100

    Was able to make the game play 5 different rounds. Just need to keep score

--- script.js
+++ script.js
@@ -13,23 +13,30 @@ function getComputerChoice() {
 // Function of playRound that gets the parameters playerSelection and computerSelection.
 // That plays a single round and declares and returns the winner.
 // Also make it accept values of upper or lower case
-let playerSelection = "rock".toLowerCase();
-let computerSelection = getComputerChoice().toLowerCase();
 let playerScore = 0;
 let computerScore = 0;
-let gameScore = 5;
+let gameScore = 0;
 
-
-function game() {
-    function playRound(playerSelection, computerSelection) {
-        if(playerSelection === 'rock' && computerSelection === 'scissors') {
-            playerScore++;
-            return `You Win! ${playerSelection} beats ${computerSelection}`; 
-        } else if(computerSelection === 'paper' && playerSelection === 'rock') {
-            computerScore++;
-            return `You Lose! ${computerSelection} beats ${playerSelection}`; 
-        } else {
-            return `You Tied Both Chose ${playerSelection}`
+function game(round) {
+    for (let round = 1; round <= 5; round++) {
+        const playerSelection = "rock".toLowerCase();
+        const computerSelection = getComputerChoice().toLowerCase();
+        function playRound(playerSelection, computerSelection) {
+            if(playerSelection === 'rock' && computerSelection === 'scissors') {
+                playerScore++;
+                gameScore++;
+                return `You Win! ${playerSelection} beats ${computerSelection}`; 
+            } else if(computerSelection === 'paper' && playerSelection === 'rock') {
+                computerScore++;
+                gameScore++;
+                return `You Lose! ${computerSelection} beats ${playerSelection}`; 
+            } else {
+                return `You Tied Both Chose ${playerSelection}`
+            }
+            console.log(playRound(playerSelection, computerSelection));
         }
+        console.log(playRound(playerSelection, computerSelection));
     }
 }
+
+game();
