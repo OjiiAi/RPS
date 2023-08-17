ff74f2b61c01daff1d954a67ae6febe958672b20
OjiiAi <osv.garciaalvarez@gmail.com>
2023-08-17 13:42:20 +0100

    Game so far runs functional and finished

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function getPlayerChoice(){
 // That plays a single round and declares and returns the winner.
 // Also make it accept values of upper or lower case
 
-
 function game() {
     let playerScore = 0;
     let computerScore = 0;
@@ -37,28 +36,46 @@ function game() {
         
         function playRound(playerSelection, computerSelection) {
             
-            if(playerSelection === 'rock' && computerSelection === 'scissors') {
-                playerScore++;
-                return `You Win! ${playerSelection} beats ${computerSelection}`; 
-            } else if(computerSelection === 'paper' && playerSelection === 'rock') {
-                computerScore++;
-                return `You Lose! ${computerSelection} beats ${playerSelection}`; 
-            } else {
-                return `You Tied Both Chose ${playerSelection}`
+            if(playerSelection === 'rock') {
+                if(computerSelection === 'scissors') {
+                    playerScore++;
+                    return `You Win! ${playerSelection} beats ${computerSelection}`
+                } else if (computerSelection === 'paper') {
+                    computerScore++
+                    return `You Lose! ${computerSelection} beats ${playerSelection}`
+                } else {
+                    return `Its a Tie! Both Sides Chose ${playerSelection}`
+                }
+            } else if(playerSelection === 'paper') {
+                if(computerSelection === 'rock') {
+                    playerScore++;
+                    return `You Win! ${playerSelection} beats ${computerSelection}`
+                } else if (computerSelection === 'scissors') {
+                    computerScore++
+                    return `You Lose! ${computerSelection} beats ${playerSelection}`
+                } else {
+                    return `Its a Tie! Both Sides Chose ${playerSelection}`
+                }
+            } else if (playerSelection === 'scissors') {
+                if(computerSelection === 'paper') {
+                    playerScore++;
+                    return `You Win! ${playerSelection} beats ${computerSelection}`
+                } else if (computerSelection === 'rock') {
+                    computerScore++
+                    return `You Lose! ${computerSelection} beats ${playerSelection}`
+                } else {
+                    return `Its a Tie! Both Sides Chose ${playerSelection}`
+                }
             }
-           
-            
         } 
         console.log(playRound(playerSelection, computerSelection));
         
     } while (playerScore < 5 && computerScore < 5);
 
     if(playerScore > computerScore) {
-        console.log("You Win!")
+        console.log("You Win! You Beat The Computer 5 Times.")
     } else {
-        console.log("You Lose")
+        console.log("You Lose! You Lost To The Computer 5 Times")
     }
-
-
 }
 game();
