87d46aa70b48724f41b3c8718ab807211a4aa4e2
OjiiAi <osv.garciaalvarez@gmail.com>
2023-08-17 13:14:47 +0100

    Game now stops when the computer or player get 5 points

--- script.js
+++ script.js
@@ -13,30 +13,34 @@ function getComputerChoice() {
 // Function of playRound that gets the parameters playerSelection and computerSelection.
 // That plays a single round and declares and returns the winner.
 // Also make it accept values of upper or lower case
-let playerScore = 0;
-let computerScore = 0;
-let gameScore = 0;
 
-function game(round) {
-    for (let round = 1; round <= 5; round++) {
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (playerScore < 5 && computerScore < 5) {
         const playerSelection = "rock".toLowerCase();
         const computerSelection = getComputerChoice().toLowerCase();
+        
         function playRound(playerSelection, computerSelection) {
+            
             if(playerSelection === 'rock' && computerSelection === 'scissors') {
                 playerScore++;
-                gameScore++;
                 return `You Win! ${playerSelection} beats ${computerSelection}`; 
             } else if(computerSelection === 'paper' && playerSelection === 'rock') {
                 computerScore++;
-                gameScore++;
                 return `You Lose! ${computerSelection} beats ${playerSelection}`; 
             } else {
                 return `You Tied Both Chose ${playerSelection}`
             }
-            console.log(playRound(playerSelection, computerSelection));
+           
         }
         console.log(playRound(playerSelection, computerSelection));
+        console.log(playerScore);
+        console.log(computerScore); 
+        
     }
-}
 
+}
 game();
